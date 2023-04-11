c94630cc48e84cf80854ebc535a780774c510bac
farildev <memmedovfaril@gmail.com>
2023-04-11 21:16:34 +0400

    UPDATE

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from "react"
+import {useState , useEffect} from "react"
 import Card from "./components/Card"
 import Header from "./components/Header"
 import "./assets/styles/style.css"
 
-
+//https://fakestoreapi.com/products
 function App() {
+
+  const [data , setData] = useState([]);
+
+  useEffect(() => { 
+    fetch("https://fakestoreapi.com/products")
+    .then((res) => res.json())
+    .then((data) => setData(data))
+  },[])
   return (
     <>
       <Header />
-      <Card />
+      {
+        data.map((index,key)=> (
+          <Card key={key} title={index.title} description={index.description} image={index.image} />
+        ))
+      }
+
     </>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import "../assets/styles/index.css"
 import "../assets/styles/card.css"
-function Card() {
+function Card({key , title , description , image}) {
   return (
     <>
     <div className="card">
         <div className="card__content container">
-            <div className="card__body">
+            <div className="card__body" key={key}>
                 <div className="card__img">
-                    <img src="" alt="" />
+                    <img src={image} alt={title} />
                 </div>
                 <div className="card__title">
-                    <h3>Salam</h3>
+                    <h3>{title}</h3>
                 </div>
                 <div className="card__description">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, natus necessitatibus! Ducimus tempora enim nemo necessitatibus, aut totam reiciendis in numquam libero sequi facere similique.</p>
+                    <p>{description}</p>
                 </div>
                 <div className="card__btn">
                     <button>Salam</button>
